ff6d584749248604ec936642af6c65102a5dc852
Lars Snipen <lars.snipen@nmbu.no>
2022-01-30 13:16:16 +0100

    Simplified orfLength

--- R/orfs.R
+++ R/orfs.R
@@ -4,40 +4,53 @@
 #' @description Finds all ORFs in prokaryotic genome sequences.
 #' 
 #' @param genome A fasta object (\code{tibble}) with the genome sequence(s).
-#' @param circular Logical indicating if the genome sequences are completed, circular sequences.
+#' @param circular Logical indicating if the genome sequences are completed, 
+#' circular sequences.
 #' @param trans.tab Translation table.
 #' 
-#' @details A prokaryotic Open Reading Frame (ORF) is defined as a subsequence starting with a  start-codon
-#' (ATG, GTG or TTG), followed by an integer number of triplets (codons), and ending with a stop-codon (TAA,
-#' TGA or TAG, unless \code{trans.tab = 4}, see below). This function will locate all such ORFs in a genome.
+#' @details A prokaryotic Open Reading Frame (ORF) is defined as a sub-sequence 
+#' starting with a  start-codon (ATG, GTG or TTG), followed by an integer number
+#' of triplets (codons), and ending with a stop-codon (TAA, TGA or TAG, unless 
+#' \code{trans.tab = 4}, see below). This function will locate all such ORFs in
+#' a genome.
 #' 
-#' The argument \code{genome} is a fasta object, i.e. a table with columns \samp{Header} and \samp{Sequence},
-#' and will typically have several sequences (chromosomes/plasmids/scaffolds/contigs).
-#' It is vital that the \emph{first token} (characters before first space) of every \samp{Header} is
-#' unique, since this will be used to identify these genome sequences in the output.
+#' The argument \code{genome} is a fasta object, i.e. a table with columns 
+#' \samp{Header} and \samp{Sequence}, and will typically have several sequences
+#' (chromosomes/plasmids/scaffolds/contigs). It is vital that the \emph{first 
+#' token} (characters before first space) of every \samp{Header} is
+#' unique, since this will be used to identify these genome sequences in the
+#' output.
 #' 
-#' An alternative translation table may be specified, and as of now the only alternative implemented is table 4.
-#' This means codon TGA is no longer a stop, but codes for Tryptophan. This coding is used by some bacteria
-#' (e.g. Mycoplasma, Mesoplasma).
+#' By default the genome sequences are assumed to be linear, i.e. contigs or
+#' other incomplete fragments of a genome. In such cases there will usually be
+#' some truncated ORFs at each end, i.e. ORFs where either the start- or the 
+#' stop-codon is lacking. In the \code{orf.table} returned by this function this
+#' is marked in the \samp{Attributes} column. The texts "Truncated=10" or 
+#' "Truncated=01" indicates truncated at the beginning or end of the genomic 
+#' sequence, respectively. If the supplied \code{genome} is a completed genome,
+#' with circular chromosome/plasmids, set the flag \code{circular = TRUE} and no
+#' truncated ORFs will be listed. In cases where an ORF runs across the origin 
+#' of a circular genome sequences, the stop coordinate will be larger than the 
+#' length of the genome sequence. This is in line with the specifications of
+#' the GFF3 format, where a \samp{Start} cannot be larger than the
+#' corresponding \samp{End}.
 #' 
-#' Note that for any given stop-codon there are usually multiple start-codons in the same reading
-#' frame. This function will return all, i.e. the same stop position may appear multiple times. If
-#' you want ORFs with the most upstream start-codon only (LORFs), then filter the output from this function
-#' with \code{\link{lorfs}}.
+#' An alternative translation table may be specified, and as of now the only
+#' alternative implemented is table 4. This means codon TGA is no longer a stop,
+#' but codes for Tryptophan. This coding is used by some bacteria
+#' (e.g. under the orders Entomoplasmatales and Mycoplasmatales).
 #' 
-#' By default the genome sequences are assumed to be linear, i.e. contigs or other incomplete fragments
-#' of a genome. In such cases there will usually be some truncated ORFs at each end, i.e. ORFs where either
-#' the start- or the stop-codon is lacking. In the \code{orf.table} returned by this function this is marked in the
-#' \samp{Attributes} column. The texts "Truncated=10" or "Truncated=01" indicates truncated at 
-#' the beginning or end of the genomic sequence, respectively. If the supplied \code{genome} is a completed genome, with 
-#' circular chromosome/plasmids, set the flag \code{circular = TRUE} and no truncated ORFs will be listed.
-#' In cases where an ORF runs across the origin of a circular genome sequences, the stop coordinate will be
-#' larger than the length of the genome sequence. This is in line with the specifications of the GFF3 format, where 
-#' a \samp{Start} cannot be larger than the corresponding \samp{End}.
+#' Note that for any given stop-codon there are usually multiple start-codons 
+#' in the same reading frame. This function will return all such nested ORFs, 
+#' i.e. the same stop position may appear multiple times. If you want ORFs with
+#' the most upstream start-codon only (LORFs), then filter the output from this
+#' function with \code{\link{lorfs}}.
 #' 
-#' @return This function returns an \code{orf.table}, which is simply a \code{\link{tibble}} with columns
-#' adhering to the GFF3 format specifications (a \code{gff.table}), see \code{\link{readGFF}}. If you want to retrieve
-#' the ORF sequences, use \code{\link{gff2fasta}}.
+#' 
+#' @return This function returns an \code{orf.table}, which is simply a 
+#' \code{\link{tibble}} with columns adhering to the GFF3 format specifications
+#' (a \code{gff.table}), see \code{\link{readGFF}}. If you want to retrieve
+#' the actual ORF sequences, use \code{\link{gff2fasta}}.
 #' 
 #' 
 #' @author Lars Snipen and Kristian Hovde Liland.
@@ -129,14 +142,15 @@ circularize <- function(ot, trans.tab){
 #' 
 #' @description Computing the lengths of all ORFs in an \code{orf.table}.
 #' 
-#' @param orf.tbl A \code{tibble} with the nine columns of the GFF-format (see \code{\link{findOrfs}}).
+#' @param orf.table A GFF-formatted \code{tibble}.
 #' @param aa Logical, length in amino acids instead of bases.
 #' 
-#' @details Computes the length of an ORF in bases, including the stop codon. However, if
-#' \code{aa = TRUE}, then the length is in amino acids after translation. This aa-length is the
-#' base-length divided by 3 and minus 1, unless the ORF is truncated and lacks a stop codon.
+#' @details By default, computes the length of an ORF in bases, including the
+#' stop codon. However, if \code{aa = TRUE}, then the length is in amino acids
+#' after translation. This aa-length is the base-length divided by 3 and minus 
+#' 1, unless the ORF is truncated and lacks a stop codon.
 #' 
-#' @return A vector of integers.
+#' @return A vector of lengths.
 #' 
 #' @author Lars Snipen and Kristian Hovde Liland.
 #' 
@@ -150,17 +164,14 @@ circularize <- function(ot, trans.tab){
 #' 
 #' @export orfLength
 #' 
-orfLength <- function(orf.tbl, aa = FALSE){
-  orf.tbl %>% 
-    mutate(Length = abs(.data$Start - .data$End) + 1) -> orf.tbl
+orfLength <- function(orf.table, aa = FALSE){
+  length <- abs(orf.table$Start - orf.table$End) + 1
   if(aa){
-    orf.tbl %>% 
-      mutate(Length = .data$Length / 3) %>% 
-      mutate(Length = if_else((str_detect(.data$Attributes, "Truncated=01") & .data$Strand == "+")
-                              |(str_detect(.data$Attributes, "Truncated=10") & .data$Strand == "-"),
-                              .data$Length, .data$Length - 1)) -> orf.tbl
+    length <- if_else((str_detect(orf.table$Attributes, "Truncated=01") & orf.table$Strand == "+")
+                      |(str_detect(orf.table$Attributes, "Truncated=10") & orf.table$Strand == "-"),
+                      length/ 3, length / 3 - 1)
   }
-  return(orf.tbl$Length)
+  return(length)
 }
 
 
